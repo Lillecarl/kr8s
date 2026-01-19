89ce3dbd8d602370971e7256bb31f7fe90ff30ee
Daniel Goldman <danielgoldman4@gmail.com>
2026-01-18 23:05:26 -0500

    feature: plumb option for validate

--- kr8s/_api.py
+++ kr8s/_api.py
@@ -15,7 +15,7 @@ import weakref
 from collections.abc import AsyncGenerator
 from contextlib import contextmanager
 from importlib.metadata import version as metadata_version
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING, Literal, Union
 
 import anyio
 import httpx
@@ -59,6 +59,7 @@ def _httpx_ws_exception_fixer():
 
 
 ApplyOpTypes = Literal["merge", "json", "strategic", "ssa"]
+ApplyValidateOption = Union[Literal["strict", "warn", "ignore"], bool]
 
 
 def _apply_op_content_type(op: ApplyOpTypes) -> str:
@@ -779,20 +780,24 @@ class Api:
         resources: list[APIObject],
         server_side: bool = False,
         force_conflicts: bool = False,
+        validate: ApplyValidateOption = "strict",
     ):
         """Use server-side apply to create or update resources."""
         async with anyio.create_task_group() as tg:
             for resource in resources:
-                tg.start_soon(resource.async_apply, server_side, force_conflicts)
+                tg.start_soon(
+                    resource.async_apply, server_side, force_conflicts, validate
+                )
 
     async def apply(
         self,
         resources: list[APIObject],
         server_side: bool = False,
         force_conflicts: bool = False,
+        validate: ApplyValidateOption = "strict",
     ):
         """Use server-side apply to create or update resources."""
-        return await self.async_apply(resources, server_side, force_conflicts)
+        return await self.async_apply(resources, server_side, force_conflicts, validate)
 
     @property
     def __version__(self) -> str:
